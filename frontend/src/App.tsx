11616729f647b8e53f629eb1b925e9b1d3768965
Nicolò Pretto <info@npretto.com>
2021-08-05 20:50:33 +0200

    hello world from monorepo with electron working

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+export const Hello = () => {
+  return <div>hello world from {`"electron/src/index.ts"`}</div>;
+};
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        fuck
+        <Route path="/" component={Hello} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,3 @@
 import React from "react";
 
-export const App = () => {
-  return <div>hello world from {`"electron/src/index.tsx"`}</div>;
-};
+export { App } from "./App";
