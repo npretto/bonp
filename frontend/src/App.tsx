61496a5db88d3575d7c0b3769141b786e4bf47d6
Nicolò Pretto <info@npretto.com>
2021-08-06 20:46:55 +0200

    y dev script (stmux) and made build -watch not go in infinite loop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 export const Hello = () => {
-  return <div>hello world from {`"electron/src/index.ts"`}</div>;
+  return <div>Hello world from {`"electron/src/index.ts"`}</div>;
 };
 
 export const App = () => {
